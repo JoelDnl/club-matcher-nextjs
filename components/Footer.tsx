f6c12548d616e646f9e12d48e269983f59a979c2
Web Development Intern <webdevelopment.intern@westernusc.ca>
2023-09-16 12:21:05 -0400

    improved footer spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,9 @@ import { SocialMedia } from "@/data/SocialMediaData";
 export default function Footer() {
   return (
     <>
-      <hr className="h-px my-8 sm:my-8 bg-slate-200 border-0"></hr>
-      <div className="flex flex-col sm:flex-row justify-between sm:mx-auto px-8 w-11/12 sm:max-w-6xl sm:pb-6 space-x-8">
-        <div className="text-center sm:text-left">
+      <hr className="h-px my-8 bg-slate-200 border-0"></hr>
+      <div className="flex flex-col sm:flex-row sm:justify-between mx-auto px-8 w-11/12 sm:max-w-6xl sm:pb-6 sm:space-x-8">
+        <div className="text-center sm:text-left w-full">
           <h3 className="font-bold">USC Main Office</h3>
           <h4 className="font-normal">
             Room 340, UCC Building
@@ -15,19 +15,18 @@ export default function Footer() {
             London, ON. N6A 3K7
           </h4>
         </div>
-        <div className="flex justify-center my-8 space-x-4 first:space-x-0">
+        <div className="inline-flex items-center justify-center sm:justify-end my-8 gap-2 mx-auto w-full">
           {SocialMedia.map((link) => {
             return (
-              <div key={link.key}>
-                <a
-                  href={link.href}
-                  type="button"
-                  target="_blank"
-                  className="text-black text-3xl"
-                >
-                  {link.icon}
-                </a>
-              </div>
+              <a
+                key={link.key}
+                href={link.href}
+                type="button"
+                target="_blank"
+                className="text-black text-3xl"
+              >
+                {link.icon}
+              </a>
             );
           })}
         </div>
