840186f8c0f35037f48c1b5a20eaae7e492551f1
Web Development Intern <webdevelopment.intern@westernusc.ca>
2023-09-17 14:52:37 -0400

    now using email to find club data

--- app/api/club/route.ts
+++ app/api/club/route.ts
@@ -1,9 +1,10 @@
-import { getClub } from "@/lib/club";
+import { NULL_CLUB, getClubByEmail } from "@/lib/club";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const data = await request.json();
-  const clubData = getClub(data);
+  if (data == undefined) return NextResponse.json({ data: NULL_CLUB });
+  const clubData = await getClubByEmail(data);
 
-  return NextResponse.json({ data: clubData });
+  return NextResponse.json({ data: clubData.data });
 }
