77c4d29ac4d7967cbfc8bab8b97b13c4f1bc6b5d
Web Development Intern <webdevelopment.intern@westernusc.ca>
2023-09-16 23:14:06 -0400

    added helper function to check valid HTTP urls

---
+++ lib/utils.ts
@@ -0,0 +1,30 @@
+export function cosineSimilarity(arrayA: number[], arrayB: number[]) {
+  let dotProduct = 0,
+    mA = 0,
+    mB = 0;
+
+  for (let i = 0; i < arrayA.length; i++) {
+    dotProduct += arrayA[i] * arrayB[i];
+    mA += arrayA[i] * arrayB[i];
+    mB += arrayB[i] * arrayB[i];
+  }
+
+  mA = Math.sqrt(mA);
+  mB = Math.sqrt(mB);
+
+  return dotProduct / (mA * mB);
+}
+
+// Borrowed from freeCodeCamp
+export function isValidHttpUrl(str: string) {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$", // fragment locator
+    "i"
+  );
+  return pattern.test(str);
+}
