39d6d9aa40541a4019f9458149c4cd37335e0e76
Web Development Intern <webdevelopment.intern@westernusc.ca>
2023-09-21 11:19:37 -0400

    changed an option for question 7

--- data/QuizData.tsx
+++ data/QuizData.tsx
@@ -69,7 +69,7 @@ export const QuizData = [
     question: "What about a dress code?",
     options: [
       "I'd like to stick to my wardrobe.",
-      "I'm malleable. I'll dress up if I have to.",
+      "I'm open-minded. I'll dress up if I have to.",
       "Any opportunity to get dressed up and I'll take it!",
     ],
   },
@@ -83,84 +83,3 @@ export const QuizData = [
     ],
   },
 ];
-
-/*
-export const QuizData = {
-  1: {
-    question:
-      "Which are the activities you would most prefer to be engaged in?",
-    options: {
-      1: "Just generally being social with other like-minded students.",
-      2: "I want to meet students of similar backgrounds.",
-      3: "I want to give back to the community through volunteer work.",
-      4: "I'm looking to enhance my educational experience.",
-      5: "I am looking to raise awareness for a particular cause.",
-    },
-  },
-  2: {
-    question: "What overall category of club would you be interested in?",
-    options: {
-      1: "Academics",
-      2: "Business/Finance",
-      3: "Community Volunteering / Philanthropy",
-      4: "Cultural",
-      5: "Health and Wellness",
-      6: "Hobbies / Special Interests",
-      7: "Music / Performing Arts",
-      8: "Politics / Social Justice",
-      9: "Sports",
-    },
-  },
-  3: {
-    question: "Are you looking to join a larger or smaller organization?",
-    options: {
-      1: "I would rather be a part of a smaller, more intimate group.",
-      2: "More than a couple dozen and I can't remember any names!",
-      3: "The bigger, the better.",
-    },
-  },
-  4: {
-    question: "What level of commitment are you prepared for?",
-    options: {
-      1: "I can only see myself being available several times in a semester.",
-      2: "A few times a month would probably be my upper limit.",
-      3: "Once or more a week would be awesome!",
-    },
-  },
-  5: {
-    question:
-      "Would you be willing to spend money throughout the year to participate in special virtual events?",
-    options: {
-      1: "I really hope to avoid spending additional money throughout the year.",
-      2: "Maybe the rare occasion.",
-      3: "Of course I would. You only live once, right?",
-    },
-  },
-  6: {
-    question:
-      "What are your feeling towards political discussions with fellow club members?",
-    options: {
-      1: "Totally not interested.",
-      2: "Would be just as happy to talk politics for the fun of it but prefer not to get carried away.",
-      3: "I am so down for discussion and debate.",
-    },
-  },
-  7: {
-    question: "What about a dress code?",
-    options: {
-      1: "I'd like to stick to my wardrobe.",
-      2: "I'm malleable. I'll dress up if I have to.",
-      3: "Any opportunity to get dressed up and I'll take it!",
-    },
-  },
-  8: {
-    question:
-      "It is important for you that any organization you join be fully accessible for those with disabilities?",
-    options: {
-      1: "Yes, absolutely.",
-      2: "Obviously it would be ideal, but it is not a particular need for me.",
-    },
-  },
-};
-
-*/
