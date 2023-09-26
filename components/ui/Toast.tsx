b9e3199ec2f163ff0ec762168887162ee76cac31
Web Development Intern <webdevelopment.intern@westernusc.ca>
2023-09-26 11:18:03 -0400

    added success toast

--- components/ui/Toast.tsx
+++ components/ui/Toast.tsx
@@ -1,14 +1,16 @@
 import { toast } from "react-toastify";
 
-interface ErrorToast {
-  position: string;
-  autoClose: number;
-  hideProgressBar: boolean;
-  closeOnClick: boolean;
-  pauseOnHover: boolean;
-  draggable: boolean;
-  progress: string | number | undefined;
-  theme: string;
+export function successToast(message: string) {
+  toast.success(message, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
 }
 
 export function errorToast(message: string) {
